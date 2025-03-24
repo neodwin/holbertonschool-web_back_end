f122e03bf6a5e723107261aa93d370c87517638b
neodwin <neo60x@hotmail.com>
2025-03-24 14:23:37 +0100

    Update task 0 & 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+      const innerTask = true;
+      const innerTask2 = false;
+      console.log(innerTask, innerTask2);
     }
   
     return [task, task2];
