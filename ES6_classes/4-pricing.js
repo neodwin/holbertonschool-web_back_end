b61afc034cfd6c874e60916874d56a5ca3c20d8d
neodwin <neo60x@hotmail.com>
2025-03-26 15:17:26 +0100

    Fix task 4 and add task 5

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,32 +1,49 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this._amount = Pricing._validateAmount(amount);
+    this._currency = Pricing._validateCurrency(currency);
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
+  set amount(newAmount) {
+    this._amount = Pricing._validateAmount(newAmount);
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
+  set currency(newCurrency) {
+    this._currency = Pricing._validateCurrency(newCurrency);
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
     return amount * conversionRate;
+  }
+
+  static _validateAmount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    return amount;
+  }
+
+  static _validateCurrency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    return currency;
   }
 }
