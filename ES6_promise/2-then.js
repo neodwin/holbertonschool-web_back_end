5745058ff4e17d6f9499ad2108d04bbd937e0736
neodwin <neo60x@hotmail.com>
2025-03-31 17:00:38 +0200

    fix identation task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({ status: 200, body: 'success' }))
     .then((response) => {
       console.log('Got a response from the API');
-      return response;
+        return response;
     })
   .catch(() => new Error());
 }
